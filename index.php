89d0022c3dd75752baa76aa3e16039cd3a3a837d
USER922-31 <user@USER922-31.local>
2026-06-05 10:55:09 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>งานที่ 1 สุพัตรา เวชชศาสตร์ BIT.2/5 เลขที่31</h1>
+    <form action="" >
+        <label for="">กรอกเลขแม่สูตรคูณ :</label> <br>
+        <input type="number" name="num" id="">
+        <input type="submit" value="คำนวณ">
+    </form>
+
+    <?php
+        if(isset($_GET["num"])){
+            $num = $_GET["num"];
+
+            echo "สูตรคูณแม่".$num;
+        }
+    ?>
+
+</body>
+</html>
