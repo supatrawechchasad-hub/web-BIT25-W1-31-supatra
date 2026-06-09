9b6e8bb0105aa326c440ef3d47d4971a5f246833
USER922-31 <user@USER922-31.local>
2026-06-09 14:28:14 +0700

    pp

--- index.php
+++ index.php
@@ -1,33 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
-    <h1>งานที่ 1 สุพัตรา เวชชศาสตร์ BIT.2/5 เลขที่31</h1>
+    <div class="card">
+        <h1>งานที่ 1 สุพัตรา เวชชศาสตร์ BIT.2/5 เลขที่31</h1>
 
-    <a href="while.php">While Loop</a>
+        <a href="while.php">While Loop</a>
 
-    <form action="" >
-        <label for="">กรอกเลขแม่สูตรคูณ :</label> <br>
-        <input type="number" name="num" id="">
-        <input type="submit" value="คำนวณ">
-    </form>
+        <form action="">
+            <label for="" class="xxx">กรอกเลขแม่สูตรคูณ :</label> <br>
+            <input class="www" type="number" name="num" id="">
+            <br>
+            <input class="eee" type="submit" value="คำนวณ">
+        </form>
 
-    <?php
-        if(isset($_GET["num"])){
-            $num = $_GET["num"];
+        <div class="aaa">
 
-            echo "สูตรคูณแม่".$num."<br>";
-            //.เริ่ม จบ เพิ่มทีละ1
-            for($i=1; $i <= 12; $i++){
-                echo $num . " x " . $i . " = " . $num * $i . "<br>";
+            <?php
+            if (isset($_GET["num"])) {
+                $num = $_GET["num"];
+                echo "สูตรคูณแม่" . $num . "<br>";
+                //.เริ่ม จบ เพิ่มทีละ1
+                for ($i = 1; $i <= 12; $i++) {
+                    echo $num . " x " . $i . " = " . $num * $i . "<br>";
+                }
             }
-        }
-    ?>
+            ?>
+        </div>
+
+    </div>
+
 
 </body>
+
 </html>
---
+++ while.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <div class="box">
+        <h1>งานที่ 1 สุพัตรา เวชชศาสตร์ BIT.2/5 เลขที่31</h1>
+
+        <a href="index.php">For Loop</a>
+
+        <form action="">
+            <label for="" class="xxx">กรอกเลขแม่สูตรคูณ :</label> <br>
+            <input class="www" type="number" name="num" id="">
+            <br>
+            <input class="eee" type="submit" value="คำนวณ">
+        </form>
+
+
+        <?php
+        if (isset($_GET["num"])) {
+            $num = $_GET["num"];
+            $i = 1;
+            while ($i <= 12) {
+                echo $num . " x " . $i . " = " . $num * $i . "<br>";
+                $i++;
+            }
+        }
+        ?>
+
+    </div>
+
+
+</body>
+
+</html>
